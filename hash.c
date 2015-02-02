41d63c9bfce1cc587e58fcd802efb19551842bf0
Christoph Schulze <chr_schulze@freenet.de>
2015-02-02 15:49:28 +0100

    some comments

--- hash.c
+++ hash.c
@@ -4,7 +4,9 @@
 #include "hash.h"
 #include "probing.h"
 
-//builds a hash table of size m, from an array of integer values
+// builds a hash table of size m, from an array of integer values
+// can call different probe functions for:
+// method:int = [0: linear probing | 1: quadratic probing]
 struct HashEntry *build_hash_table(int values[], unsigned int m, unsigned int length, int method) {
 
     struct HashEntry *hashTable = (struct HashEntry*)malloc(sizeof(struct HashEntry)*m);
@@ -23,7 +25,9 @@ struct HashEntry *build_hash_table(int values[], unsigned int m, unsigned int le
     return hashTable;
 }
 
-//returning the key for inserting a new value into a hash table
+// returning the key for inserting a new value into a hash table.
+// can call different probe functions for:
+// method:int = [0: linear probing | 1: quadratic probing]
 int getKey(struct HashEntry hashTable[], int value, int m, int method) {
     int key = 0, j = 0, counter = 0;
 
@@ -35,8 +39,9 @@ int getKey(struct HashEntry hashTable[], int value, int m, int method) {
     return key;
 }
 
-//creating a hash value from a given integer value (k).
-//can also use different probe functions [0: linear probing, 1: quadratic probing]
+// creating a hash value from a given integer value (k).
+// can also use different probe functions for:
+// method:int = [0: linear probing | 1: quadratic probing]
 int hash(int k, unsigned int j, unsigned int m, int method) {
     int probing = 0;
     if (0 == method)
@@ -47,11 +52,12 @@ int hash(int k, unsigned int j, unsigned int m, int method) {
     return (hash_division_rest(k, m) - probing) % m;
 }
 
+// create hash of integer value by division rest method
 unsigned int hash_division_rest(int k, unsigned int m) {
     return k % m;
 }
 
-//converting a string into a number by the horner schema
+// converting a string into a number by the horner schema
 long unsigned int horner(char *key, unsigned int length) {
     char *characters = key;
 
